5be247204735eaebac281a111637e458e928daa3
Kaushik Das <kudokaito.pd@gmail.com>
2026-01-15 20:31:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import pickle
 import tensorflow as tf
 
 # Load trained ANN model
-model = tf.keras.models.load_model("saved_model\churn_model.h5")
+model = tf.keras.models.load_model("saved_model/churn_model.h5")
 
 # Load preprocessor (ColumnTransformer pipeline)
-with open("saved_model\preprocessor.pkl", "rb") as f:
+with open("saved_model/preprocessor.pkl", "rb") as f:
     preprocessor = pickle.load(f)
 
 # Streamlit UI
